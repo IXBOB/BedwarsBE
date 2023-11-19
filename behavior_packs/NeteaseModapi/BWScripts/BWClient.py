c361df3eafa13f993b5340928f235ac1577ed5c5
IXBOB <66829241+IXBOB@users.noreply.github.com>
2023-11-19 08:18:47 +0800

    Refactoring for CN version of C key amplification

--- behavior_packs/NeteaseModapi/BWScripts/BWClient.py
+++ behavior_packs/NeteaseModapi/BWScripts/BWClient.py
@@ -26,7 +26,35 @@ class BWClient(clientApi.GetClientSystemCls()):
         
         
     def ClientOnKeyPressInGame(self, args):
-        self.NotifyToServer('ClientOnKeyPressInGame', args)
+        #获取基本数据   
+        screenName = args['screenName']
+        self.key = args['key']
+        self.isDown = args['isDown']
+        
+        #!--实现C键放大--!
+        comp = clientApi.GetEngineCompFactory().CreateConfigClient(clientApi.GetLevelId())
+        getStoredFov = comp.GetConfigData("StoredFovConfig", False).get("StoredFov","Empty")
+        
+        
+        if self.key == '67' and self.isDown == '1' and screenName == 'hud_screen':
+        #按下C键后更改Fov
+            if getStoredFov == "Empty": 
+            #如果从未获取过玩家的Fov值，则获取，用来存储按下C键前的Fov值
+                comp = clientApi.GetEngineCompFactory().CreateCamera(-1)
+                currentFov = comp.GetFov()
+                comp = clientApi.GetEngineCompFactory().CreateConfigClient(clientApi.GetLevelId())
+                data = {}
+                data["StoredFov"] = currentFov
+                comp.SetConfigData("StoredFovConfig", data, False)
+            comp = clientApi.GetEngineCompFactory().CreateCamera(-1)
+            comp.SetFov(25)
+        if self.key == '67' and self.isDown == '0' or screenName != "hud_screen":
+        #释放C键后读取按下C键前的Fov值并设置
+            comp = clientApi.GetEngineCompFactory().CreateConfigClient(clientApi.GetLevelId())
+            fovBefore = comp.GetConfigData("StoredFovConfig", False).get("StoredFov","Empty")
+            comp = clientApi.GetEngineCompFactory().CreateCamera(-1)
+            comp.SetFov(fovBefore)
+
         
     def MenuUI(self):
         clientApi.RegisterUI("BWSripts", "MenuUI", "BWScripts.uiScript.MenuUIScreenNode", "MenuUI.screen")
--- behavior_packs/NeteaseModapi/BWScripts/BWServer.py
+++ behavior_packs/NeteaseModapi/BWScripts/BWServer.py
@@ -31,38 +31,7 @@ class BWServer(serverApi.GetServerSystemCls()):
         
         
 
-    def OnKeyPressInGame(self, args):
-        #获取基本数据
-        id = args['__id__']
-        screenName = args['screenName']
-        self.key = args['key']
-        self.isDown = args['isDown']
-        
-        #!--实现C键放大--!
-        comp = serverApi.GetEngineCompFactory().CreateExtraData(id)
-        getStoredFov = comp.GetExtraData("fovBefore")
-        
-
-        
-        
-        if self.key == '67' and self.isDown == '1' and screenName == 'hud_screen':
-        #按下C键后更改Fov
-            if getStoredFov is None: 
-            #如果从未获取过玩家的Fov值，则获取，用来存储按下C键前的Fov值
-                comp = clientApi.GetEngineCompFactory().CreateCamera(-1)
-                currentFov = comp.GetFov()
-                entitycomp = serverApi.GetEngineCompFactory().CreateExtraData(id)
-                entitycomp.SetExtraData("fovBefore",currentFov)
-            comp = clientApi.GetEngineCompFactory().CreateCamera(-1)
-            comp.SetFov(25)
-        if self.key == '67' and self.isDown == '0' and screenName == 'hud_screen':
-        #释放C键后读取按下C键前的Fov值并设置
-            comp = serverApi.GetEngineCompFactory().CreateExtraData(id)
-            fovBefore = comp.GetExtraData("fovBefore")
-            comp = clientApi.GetEngineCompFactory().CreateCamera(-1)
-            comp.SetFov(fovBefore)
-            entitycomp = serverApi.GetEngineCompFactory().CreateExtraData(id)
-            entitycomp.CleanExtraData("fovBefore")
+    # def OnKeyPressInGame(self, args):
 
 
     
